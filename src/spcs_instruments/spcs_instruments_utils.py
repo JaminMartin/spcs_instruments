059168e30a8f0e9d044aeaec092d4d1879b7b7f2
Jamin Martin <jamin.martin1@gmail.com>
2024-05-23 15:43:42 +1200

    added config reading

---
+++ src/spcs_instruments/spcs_instruments_utils.py
@@ -0,0 +1,14 @@
+from time import sleep
+import time
+import datetime
+import os
+import toml
+
+
+
+
+
+def load_config(path: str) -> dict:
+    with open(path, 'r') as f:
+        config_toml = toml.load(f)
+    return config_toml    
