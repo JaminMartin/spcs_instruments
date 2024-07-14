00a982424d8a740389d0723febcfe649ce8faec4
Jamin Martin <jamin.martin1@gmail.com>
2024-07-15 08:38:49 +1200

    basis of cli structure setup

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,17 +1,24 @@
 use clap::Parser;
 use pyo3::prelude::*;
+use std::path::PathBuf;
+use std::process::Command;
 
-/// Simple program to greet a person
+/// A commandline experiment manager for SPCS-Instruments
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Name of the person to greet
+    /// Email address to recieve results
     #[arg(short, long)]
-    name: String,
-
-    /// Number of times to greet
+    email: Option<String>,
+    /// Time delay in minutes before starting the experiment
+    #[arg(short, long, default_value_t = 0)]
+    delay: u8,
+    // Number of times to loop the experiment
     #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    loops: u8,
+    // Path to the python file containing the experimental setup
+    #[arg(short, long)]
+    path: PathBuf,
 }
 
 #[pyfunction]
@@ -20,7 +27,11 @@ pub fn cli_parser() {
     // For debuging purposes to see what arguments python injects
     // Get original arguments
     // println!("Original arguments: {:?}", original_args);
-
+    let python_path: Option<&String> = original_args.iter().find(|arg| arg.contains("python"));
+    let python_path_str = python_path
+        .map(|s| s.clone())
+        .unwrap_or_else(|| "".to_string());
+    // println!("{}", python_path_str);
     // Filter out unwanted arguments
     let mut cleaned_args: Vec<String> = original_args
         .into_iter()
@@ -29,15 +40,33 @@ pub fn cli_parser() {
 
     // Replace the first argument after cleaning with the actual cli tool name currently this is a place holder
     if let Some(first_arg_index) = cleaned_args.iter().position(|arg| !arg.starts_with('-')) {
-        cleaned_args[first_arg_index] = "cake".to_string();
+        cleaned_args[first_arg_index] = "pfx".to_string();
     }
 
     // Parse the cleaned arguments using Clap
     let args = Args::parse_from(cleaned_args);
 
     // Print greetings based on parsed arguments
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name);
+    if !python_path_str.is_empty() {
+        // Path to the Python script you want to execute
+        let script_path = args.path;
+
+        // Execute the Python script
+        let output = Command::new(&python_path_str)
+            .arg(script_path)
+            .output()
+            .expect("Failed to execute Python script");
+
+        // Handle the output
+        if output.status.success() {
+            let stdout = String::from_utf8_lossy(&output.stdout);
+            println!("Script output:\n{}", stdout);
+        } else {
+            let stderr = String::from_utf8_lossy(&output.stderr);
+            eprintln!("Script error:\n{}", stderr);
+        }
+    } else {
+        eprintln!("No Python path found in the arguments");
     }
 }
 
