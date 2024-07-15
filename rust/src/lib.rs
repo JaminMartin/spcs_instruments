baeb460770c677c0dd3a11d02601d111acf8d16b
Jamin Martin <jamin.martin1@gmail.com>
2024-07-15 15:54:33 +1200

    got most features bar attachment working.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,7 +1,52 @@
 use clap::Parser;
+use lettre::message::{header::ContentType, Attachment, MultiPart, SinglePart};
+use lettre::{Message, SmtpTransport, Transport};
 use pyo3::prelude::*;
+use std::fs;
 use std::path::PathBuf;
 use std::process::Command;
+use std::thread::sleep;
+use std::time::Duration;
+
+pub fn mailer(email_adr: Option<&String>) {
+    match email_adr {
+        Some(email) => {
+            let filename = String::from("spcs_experiment_20240715_121900.log");
+            let filebody =
+                fs::read("/home/jamin/Documents/mailer/spcs_experiment_20240715_121900.log")
+                    .expect("Cant find file!");
+            let content_type = ContentType::parse("text/plain").unwrap();
+            let attachment = Attachment::new(filename).body(filebody, content_type);
+            let email = Message::builder()
+                .from("PyFeX <PyFex@canterbury.ac.nz>".parse().unwrap())
+                .reply_to("PyFeX <PyFex@canterbury.ac.nz>".parse().unwrap())
+                .to(email.parse().unwrap())
+                .subject("Experiment Notification")
+                .header(ContentType::TEXT_PLAIN)
+                .multipart(
+                    MultiPart::mixed()
+                        .singlepart(
+                            SinglePart::builder()
+                                .header(ContentType::TEXT_HTML)
+                                .body(String::from("Experimental Results Attached!")),
+                        )
+                        .singlepart(attachment),
+                )
+                .unwrap();
+
+            let mailer = SmtpTransport::builder_dangerous("smtphost.canterbury.ac.nz").build();
+
+            // Send the email
+            match mailer.send(&email) {
+                Ok(_) => println!("Email sent successfully!"),
+                Err(e) => panic!("Could not send email: {e:?}"),
+            }
+        }
+        None => {
+            // Do nothing if no email address is provided
+        }
+    }
+}
 
 /// A commandline experiment manager for SPCS-Instruments
 #[derive(Parser, Debug)]
@@ -12,7 +57,7 @@ struct Args {
     email: Option<String>,
     /// Time delay in minutes before starting the experiment
     #[arg(short, long, default_value_t = 0)]
-    delay: u8,
+    delay: u64,
     // Number of times to loop the experiment
     #[arg(short, long, default_value_t = 1)]
     loops: u8,
@@ -46,24 +91,29 @@ pub fn cli_parser() {
     // Parse the cleaned arguments using Clap
     let args = Args::parse_from(cleaned_args);
 
-    // Print greetings based on parsed arguments
+    // Call the python interp and and run the experiment as per additional args
+
+    println!("Experiment starting in {} s", args.delay * 60);
+    sleep(Duration::from_secs(&args.delay * 60));
     if !python_path_str.is_empty() {
         // Path to the Python script you want to execute
         let script_path = args.path;
+        for _ in 0..args.loops {
+            // Execute the Python script
+            let output = Command::new(&python_path_str)
+                .arg(&script_path)
+                .output()
+                .expect("Failed to execute Python script");
 
-        // Execute the Python script
-        let output = Command::new(&python_path_str)
-            .arg(script_path)
-            .output()
-            .expect("Failed to execute Python script");
-
-        // Handle the output
-        if output.status.success() {
-            let stdout = String::from_utf8_lossy(&output.stdout);
-            println!("Script output:\n{}", stdout);
-        } else {
-            let stderr = String::from_utf8_lossy(&output.stderr);
-            eprintln!("Script error:\n{}", stderr);
+            // Handle the output
+            if output.status.success() {
+                let stdout = String::from_utf8_lossy(&output.stdout);
+                println!("Script output:\n{}", stdout);
+            } else {
+                let stderr = String::from_utf8_lossy(&output.stderr);
+                eprintln!("Script error:\n{}", stderr);
+            }
+            mailer(args.email.as_ref())
         }
     } else {
         eprintln!("No Python path found in the arguments");
