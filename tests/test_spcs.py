5af8416d604b8e8ea93f799dfe66721056f77197
JaminMartin <jamin.martin1@gmail.com>
2024-07-18 19:59:12 +1200

    updated tests for 0.2.0 release

--- tests/test_spcs.py
+++ tests/test_spcs.py
@@ -1,7 +1,5 @@
 import os
 import sys
-import pytest
-import numpy as np
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
@@ -9,14 +7,14 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 from src.spcs_instruments.instruments.test_instrument import Fake_daq
 from src.spcs_instruments.spcs_instruments_utils import Experiment
 
-
+file_name = ".exp_output.log"
 def test_fake_experiment():
     def a_measurement(config) -> dict:
         daq = Fake_daq(config)
         for i in range(5):
-           val = daq.measure()
-           print(val)
-        
+            val = daq.measure()
+            print(val)
+
         data = {daq.name: daq.data}
         return data
 
@@ -27,9 +25,18 @@ def test_fake_experiment():
     experiment = Experiment(a_measurement, config_path)
     experiment.start()
 
-    with open(experiment.log_file.name, "r") as f:
+    with open(file_name, "r") as f:
         log_contents = f.read()
-    assert "Experiment started at" in log_contents
-    assert "Experiment information" in log_contents
-    assert "Measurement data for simulated daq:" in log_contents
-    assert "Experiment ended at" in log_contents
+    assert "[experiment]" in log_contents
+    assert "start_time" in log_contents
+    assert "[Test_DAQ.data]" in log_contents
+    assert "current =" in log_contents
+
+
+    try:
+        os.remove(file_name)
+        print(f"File '{file_name}' deleted successfully")
+    except FileNotFoundError:
+        print(f"File '{file_name}' not found")
+    except Exception as e:
+        print(f"Failed to delete file '{file_name}': {e}")
