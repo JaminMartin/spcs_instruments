5198fca6ecaab5000b8b99b3af28c95708e1f6eb
Jamin Martin <jamin.martin1@gmail.com>
2024-08-30 15:37:20 +1200

    Added working keithley support. Still needs work.

---
+++ examples/keithley_example.py
@@ -0,0 +1,13 @@
+# Code to execute when the script is run directly
+from spcs_instruments import Keithley2400
+import os
+import sys
+
+if __name__ == "__main__":
+    # Example usage
+    dir_path = os.path.dirname(os.path.abspath(__file__))
+    config_path = os.path.join(dir_path, "..", "templates", "config4.toml")
+    keithley = Keithley2400(config_path)
+    keithley.measure()
+    
+    keithley.close()
--- src/spcs_instruments/instruments/__init__.py
+++ src/spcs_instruments/instruments/__init__.py
@@ -1,4 +1,4 @@
 from .test_instrument import Fake_daq
 from .siglent_sds_2352xe_driver import SiglentSDS2352XE
-
-__all__ = ["Fake_daq", "SiglentSDS2352XE"]
+from .keithley_2400_driver import Keithley2400
+__all__ = ["Fake_daq", "SiglentSDS2352XE", "Keithley2400"]
