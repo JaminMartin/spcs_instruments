089f24e7d2fa1652f70bf6404b8fdfc8e1ea8c0f
David Walker <david.walker@pg.canterbury.ac.nz>
2024-09-12 14:48:01 +1200

    Fixed Keithley, Prints a dictionary of data

--- examples/keithley_example.py
+++ examples/keithley_example.py
@@ -8,6 +8,12 @@ if __name__ == "__main__":
     dir_path = os.path.dirname(os.path.abspath(__file__))
     config_path = os.path.join(dir_path, "..", "templates", "config4.toml")
     keithley = Keithley2400(config_path)
-    keithley.measure()
+
+    for i in range(1,11):
+        keithley.measure()
+
+
+
+    print(keithley.data)
     
     keithley.close()
