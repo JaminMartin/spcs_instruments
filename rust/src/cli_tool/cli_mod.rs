e084ceab0f84720dc2fb393e0ac179a573712582
Jamin Martin <jamin.martin1@gmail.com>
2024-11-12 20:09:16 +1300

    updated to support tcp data streaming

--- rust/src/cli_tool/cli_mod.rs
+++ rust/src/cli_tool/cli_mod.rs
@@ -1,32 +1,21 @@
-use crate::data_handler::{create_explog_file, create_time_stamp, process_output};
-use crate::mail_handler::mailer;
+use crate::data_handler::{create_time_stamp, ServerState};
+use crate::tcp_handler::{save_state, server_status, start_tcp_server};
 use clap::Parser;
+use crossbeam::channel;
 use env_logger::{Builder, Target};
 use log::LevelFilter;
 use pyo3::prelude::*;
 use std::env;
-use std::fs;
+use std::fmt::Debug;
 use std::io::{self, BufRead};
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::process::{Command, Stdio};
+use std::sync::Arc;
+use std::thread;
 use std::thread::sleep;
 use std::time::Duration;
-
-fn get_current_dir() -> String {
-    env::current_dir()
-        .unwrap_or_else(|_| PathBuf::from("."))
-        .to_str()
-        .unwrap()
-        .to_string()
-}
-
-fn resolve_path(path: &Path) -> PathBuf {
-    if path.is_absolute() {
-        path.to_path_buf()
-    } else {
-        env::current_dir().unwrap().join(path)
-    }
-}
+use tokio::sync::broadcast;
+use tokio::sync::Mutex;
 
 /// A commandline experiment manager for SPCS-Instruments
 #[derive(Parser, Debug)]
@@ -54,14 +43,6 @@ struct Args {
 
 #[pyfunction]
 pub fn cli_parser() {
-    // Initialises a temp file for writing experimental data to that can be accessed by other functions. Ensures that if cleanup has failed then logs arent appended into the same file in the next run
-
-    let temp_filename = ".exp_output.log";
-    match create_explog_file(temp_filename) {
-        Ok(_) => {}
-        Err(e) => eprintln!("Error creating experimental logfile file: {}", e),
-    }
-
     // Placeholder fix to allow spcs-instruments to work on windows. Rye does not seem to path correctly on non-unix based
     // systems. This is a work around based on rye's internal structure. It is a hotfix and is by no means "correct" however, it does work.
     let original_args: Vec<String> = std::env::args().collect();
@@ -122,52 +103,101 @@ pub fn cli_parser() {
         .target(Target::Stdout)
         .format_timestamp_secs();
     builder.init();
+
     log::info!(target: "pfx", "Experiment starting in {} s", args.delay * 60);
     sleep(Duration::from_secs(&args.delay * 60));
-
-    if !python_path_str.is_empty() {
-        let script_path = args.path;
+    let python_path = Arc::new(python_path_str);
+    let script_path = Arc::new(args.path);
+    let python_path_loop = Arc::clone(&python_path);
+    let output_path = Arc::new(args.output);
+    if !python_path_loop.is_empty() {
         for _ in 0..args.loops {
+            let python_path_clone = Arc::clone(&python_path);
+            let script_path_clone = Arc::clone(&script_path);
+            log::info!("Server is starting...");
+            let (tx, rx) = channel::unbounded();
+            let state = Arc::new(Mutex::new(ServerState::new()));
+            let (shutdown_tx, _) = broadcast::channel(1);
+            let shutdown_rx_tcp = shutdown_tx.subscribe();
+            let shutdown_rx_server_satus = shutdown_tx.subscribe();
+            let shutdown_rx_logger = shutdown_tx.subscribe();
+
+            let tcp_state = Arc::clone(&state);
+            let server_state = Arc::clone(&state);
+            let tcp_tx = tx.clone();
+
+            let tcp_server_thread = thread::spawn(move || {
+                let addr = "127.0.0.1:8080";
+                let rt = tokio::runtime::Runtime::new().unwrap();
+                rt.block_on(start_tcp_server(tcp_tx, addr, tcp_state, shutdown_rx_tcp))
+                    .unwrap();
+            });
+            let python_thread = thread::spawn(move || {
+                start_python_process(python_path_clone, script_path_clone).unwrap();
+                shutdown_tx.send(()).unwrap();
+            });
+
+            let printer_thread = thread::spawn(move || {
+                let rt = tokio::runtime::Runtime::new().unwrap();
+                rt.block_on(server_status(server_state, shutdown_rx_server_satus))
+                    .unwrap();
+            });
+            let save_statie_arc = Arc::clone(&state);
             let file_name_suffix = create_time_stamp(true);
-            // Execute the Python script
-            let mut output = Command::new(&python_path_str)
-                .arg("-u")
-                .arg(&script_path)
-                .stdout(Stdio::piped())
-                .spawn()
-                .expect("Failed to execute Python script");
 
-            // Handle the output
-            if let Some(stdout) = output.stdout.take() {
-                let reader = io::BufReader::new(stdout);
-                for line in reader.lines() {
-                    match line {
-                        Ok(line) => log::debug!(target: "python", "{}", line),
-                        Err(e) => log::error!(target: "python" , "Error reading line: {}", e),
-                    }
-                }
+            let output_path_clone = Arc::clone(&output_path);
+            let dumper = thread::spawn(move || {
+                let rt = tokio::runtime::Runtime::new().unwrap();
+                rt.block_on(save_state(
+                    save_statie_arc,
+                    shutdown_rx_logger,
+                    &file_name_suffix,
+                    output_path_clone.as_ref(),
+                ))
+                .unwrap();
+            });
+
+            for received in rx.try_iter() {
+                log::debug!("Received data: {}", received);
             }
-            let output_path: PathBuf = resolve_path(Path::new(&args.output));
-            let output_file = match process_output(&output_path, &file_name_suffix) {
-                Ok(v) => Ok(v),
-                Err(e) => {
-                    log::error!(target: "pfx", "{:?}", e);
-
-                    Err(e)
-                }
-            };
-            log::info!(target: "pfex", "The output file directory is: {}", output_path.display());
-
-            mailer(args.email.as_ref(), &output_path, &output_file);
+            tcp_server_thread.join().unwrap();
+            python_thread.join().unwrap();
+            printer_thread.join().unwrap();
+            dumper.join().unwrap();
+            log::info!(target: "pfx", "The output file directory is: {}", output_path);
+            // TODO! fix mailer for new structure.
+            //mailer(args.email.as_ref(), &output_path, &output_file);
         }
     } else {
         log::error!(target: "pfx","No Python path found in the arguments");
     }
+}
 
-    match fs::remove_file(temp_filename) {
-        Ok(_) => {
-            log::info!(target: "pfx", "Experiment exited, Cleaned up temp files")
+fn get_current_dir() -> String {
+    env::current_dir()
+        .unwrap_or_else(|_| PathBuf::from("."))
+        .to_str()
+        .unwrap()
+        .to_string()
+}
+
+fn start_python_process(python_path: Arc<String>, script_path: Arc<PathBuf>) -> io::Result<()> {
+    let mut python_process = Command::new(python_path.as_ref())
+        .arg("-u")
+        .arg(script_path.as_ref())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Failed to execute Python script");
+
+    // Handle the output
+    if let Some(stdout) = python_process.stdout.take() {
+        let reader = io::BufReader::new(stdout);
+        for line in reader.lines() {
+            match line {
+                Ok(line) => log::debug!(target: "python", "{}", line),
+                Err(e) => log::error!("Error reading line: {}", e),
+            }
         }
-        Err(_e) => log::info!(target: "pfx", "Nothing to clean up! Experiment complete",),
     }
+    Ok(())
 }
