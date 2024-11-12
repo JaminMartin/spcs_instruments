5f8c8b74e6fbfb35316901eb8051ce40ffa482c2
Jamin Martin <jamin.martin1@gmail.com>
2024-11-12 20:08:44 +1300

    removal of non-needed python functions

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,16 +1,17 @@
 pub mod cli_tool;
 pub mod data_handler;
 pub mod mail_handler;
+pub mod tcp_handler;
 use pyo3::prelude::*;
 
 use cli_tool::cli_parser;
-use data_handler::{load_experimental_data, start_experiment, update_experiment_log};
+use data_handler::load_experimental_data;
 
 #[pymodule]
 pub fn pyfex(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(cli_parser, m)?)?;
-    m.add_function(wrap_pyfunction!(start_experiment, m)?)?;
-    m.add_function(wrap_pyfunction!(update_experiment_log, m)?)?;
+    // m.add_function(wrap_pyfunction!(start_experiment, m)?)?;
+    // m.add_function(wrap_pyfunction!(update_experiment_log, m)?)?;
     m.add_function(wrap_pyfunction!(load_experimental_data, m)?)?;
     Ok(())
 }
