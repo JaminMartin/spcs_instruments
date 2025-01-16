69f2d0e196d73b9e431b14c0126d36ac88b02eb4
Jamin Martin <jamin.martin1@gmail.com>
2025-01-17 07:52:49 +1300

    Added automatic data constructs for instruments

--- src/spcs_instruments/instruments/cryostats/scryostation_driver.py
+++ src/spcs_instruments/instruments/cryostats/scryostation_driver.py
@@ -1,9 +1,4 @@
 
-import time
-import datetime
-import argparse
-import sys
-import os
 from ...spcs_instruments_utils import load_config, pyfex_support
 from .montana_support import instrument, scryostation
 from .montana_support.instrument import TunnelError
@@ -21,7 +16,6 @@ class Scryostation:
         self.config = config.get('device', {}).get(self.name, {})
         self.ip = self.config.get("device_ip")
         self.cryostat = scryostation.SCryostation(self.ip)
-        self.init_time = time.time()
         print(f"{self.name} connected with this config {self.config}")
         self.sock = self.tcp_connect()
             
@@ -31,8 +25,7 @@ class Scryostation:
                 "temperature (K)": [],
                 "stability (K)": [],
                 "Pressure (Pa)": [],
-                "time since initialisation (s)": [],
-            }
+             }
     def setup_config(self):
 
         print("Initialising cryostat into desired state")
@@ -41,11 +34,9 @@ class Scryostation:
         values = self.cryostat.get_platform_temperature_sample()
         values_pressure = self.cryostat.get_sample_chamber_pressure()
         self.cryostat.get_sample_chamber_pressure()
-        elapsed_time = time.time() - self.init_time
         self.data["temperature (K)"] = [values["temperature"]]
         self.data["stability (K)"] = [values["temperatureStability"]]
         self.data["Pressure (Pa)"] = [values_pressure]
-        self.data["time since initialisation (s)"] = [elapsed_time]
         
         payload = self.create_payload()
         print(payload)
--- src/spcs_instruments/spcs_instruments_utils.py
+++ src/spcs_instruments/spcs_instruments_utils.py
@@ -1,6 +1,8 @@
 import toml
 import socket
 import json
+import time
+from functools import wraps
 
 def load_config(path: str) -> dict:
     with open(path, "r") as f:
@@ -10,7 +12,15 @@ def load_config(path: str) -> dict:
 
 
 def pyfex_support(cls):
-    
+    instrument_init = cls.__init__
+
+    @wraps(instrument_init)
+    def extension_init(self, *args, **kwargs):
+        self.init_time_s = time.time()
+        instrument_init(self, *args, **kwargs)    
+
+
+             
     def tcp_connect(self, host='127.0.0.1', port=8080):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         
@@ -36,6 +46,8 @@ def pyfex_support(cls):
 
     def create_payload(self) -> dict:
         device_config = {key: value for key, value in self.config.items()}
+        elapsed_time = time.time() - self.init_time_s
+        self.data["time since initialisation (s)"] = [elapsed_time]
         
         payload = {
             "device_name": self.name,
@@ -47,6 +59,8 @@ def pyfex_support(cls):
     cls.create_payload = create_payload
     cls.tcp_connect = tcp_connect
     cls.tcp_send = tcp_send
+
+    cls.__init__ = extension_init
     return cls
     
 @pyfex_support    
