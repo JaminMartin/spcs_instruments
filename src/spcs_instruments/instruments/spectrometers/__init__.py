7218f4820f5005fd5d3a99b8609ec1d99af75482
Jamin Martin <jamin.martin1@gmail.com>
2025-04-02 15:11:29 +1300

    added ocean optics spectrometer support v1

--- src/spcs_instruments/instruments/spectrometers/__init__.py
+++ src/spcs_instruments/instruments/spectrometers/__init__.py
@@ -1,3 +1,4 @@
 from .Horiba_iHR550_spectrometer_driver import HoribaiHR550
 from .test_spectrometer import Test_spectrometer
-__all__ = ["HoribaiHR550","Test_spectrometer"]
+from .oceanoptics_driver import Ocean_optics_spectrometer
+__all__ = ["HoribaiHR550","Test_spectrometer", "Ocean_optics_spectrometer"]
